ed68f9d5cb78cafd6e25b8df8222e1a3a8113a7c
yoshiki <katoyoshi9814@gmail.com>
2021-01-30 13:11:33 +0900

    入力フォーム追加機能の修正

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Form from './Form'
 import List from './List'
 
 const App = () => {
-    const todos = [
-        {
+    const [todos, setTodos] = useState(
+        [{
             content: '課題をする'
         },
         {
@@ -15,12 +15,25 @@ const App = () => {
         },
         {
             content: '寝る'
-        }
-    ]
+        }])
+    // const todos = [
+    //     {
+    //         content: '課題をする'
+    //     },
+    //     {
+    //         content: '料理を作る'
+    //     },
+    //     {
+    //         content: '作った料理を食べる'
+    //     },
+    //     {
+    //         content: '寝る'
+    //     }
+    // ]
     return (
         <>
             <h1>TODO APP</h1>
-            <Form />
+            <Form todos={todos} setTodos={setTodos}/>
             <List todos={todos} />
         </>
     )
--- src/Form.js
+++ src/Form.js
@@ -1,8 +1,33 @@
-import React from 'react'
-const Form = () => {
+import React, {useState} from 'react'
+
+const Form = ({ todos, setTodos }) => {
+    const [value, setValue] = useState("")
+    const handleSubmit= (e) => {
+        // formの内容をtodoの配列に追加する処理を書く
+        e.preventDefault()
+        // console.log(...todos);
+        if (value == "") {
+            alert('内容が空です')
+            return;
+        }
+        setTodos([
+            ...todos,
+            {
+                content: value
+            }
+        ])
+        setValue("")
+    }
     return (
-        <form>
-            <input type="text" />
+        <form onSubmit={handleSubmit}>
+            <input
+            value={value}
+            type="text"
+            onChange={e => {
+                // console.log(e.target.value)
+                setValue(e.target.value)
+            }}
+            />
         </form>
     )
 }
